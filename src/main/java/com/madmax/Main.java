d91d78b53d866ceea6b17f21d80615d2873eaa5f
TristanBoulesteix <tristan.boulesteix@gmail.com>
2019-09-23 10:47:57 +0200

    Update Main.java

--- src/main/java/com/madmax/Main.java
+++ src/main/java/com/madmax/Main.java
@@ -2,6 +2,6 @@ package com.madmax;
 
 public class Main {
     public static void main (String[] args){
-        System.out.println("titi le boss !");
+        System.out.println("Hello world !");
     }
 }
