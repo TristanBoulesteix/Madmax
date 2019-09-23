855f2c81315cf1197e6205eff0e97482e0f50432
TristanBoulesteix <tristan.boulesteix@gmail.com>
2019-09-23 13:45:18 +0200

    Files done

--- src/main/java/com/madmax/model/Files.java
+++ src/main/java/com/madmax/model/Files.java
@@ -1,5 +1,7 @@
 package com.madmax.model;
 
+import java.io.*;
+
 public class Files {
     private final static Files INSTANCE = new Files();
 
@@ -10,10 +12,34 @@ public class Files {
     }
 
     public String getData(String path) {
+        StringBuilder builder = new StringBuilder();
 
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(new File(path)));
+
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                builder.append(line);
+            }
+
+            reader.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return builder.toString();
     }
 
-    public void setData(String path) {
+    public void setData(String path, String data) {
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(new File(path)));
+            writer.write(data);
+            writer.flush();
+            writer.close();
 
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
