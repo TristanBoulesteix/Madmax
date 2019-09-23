7fa8f5191c04318b3d4e9c92908dddec7cf97c89
TristanBoulesteix <tristan.boulesteix@gmail.com>
2019-09-23 16:36:48 +0200

    Update Xor

--- src/main/java/com/madmax/Main.java
+++ src/main/java/com/madmax/Main.java
@@ -1,7 +1,10 @@
 package com.madmax;
 
+import com.madmax.model.Decrypt;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world !");
+        Decrypt.getInstance().decrypt("toto", "key");
     }
 }
--- src/main/java/com/madmax/model/Decrypt.java
+++ src/main/java/com/madmax/model/Decrypt.java
@@ -13,8 +13,14 @@ public class Decrypt {
     public String decrypt(String data, String key) {
         StringBuilder builder = new StringBuilder();
 
-        for (char letter : data.toCharArray()) {
+        int keyIndex = 0;
 
+        for (char letter : data.toCharArray()) {
+            int binaryLetter = letter;
+            int binaryKeyLetter = key.charAt(keyIndex);
+
+            int a = binaryLetter ^ binaryKeyLetter;
+            System.out.println((char) a);
         }
 
         return builder.toString();
