1747686941a02b751d2a4f8b5dd420c9eeed6830
TristanBoulesteix <tristan.boulesteix@gmail.com>
2019-09-23 11:45:05 +0200

    Create Files.java

---
+++ src/main/java/com/madmax/model/Files.java
@@ -0,0 +1,19 @@
+package com.madmax.model;
+
+public class Files {
+    private final static Files INSTANCE = new Files();
+
+    private Files() { }
+
+    public static Files getInstance() {
+        return INSTANCE;
+    }
+
+    public String getData(String path) {
+
+    }
+
+    public void setData(String path) {
+
+    }
+}
