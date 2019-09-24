3e311c0399406d6314a5a75c14ebe88182640773
VincentOnfray <vincent.onfray@viacesi.fr>
2019-09-24 10:47:57 +0200

    nsuqefuesnf

--- src/main/java/com/madmax/view/AuthFrame.java
+++ src/main/java/com/madmax/view/AuthFrame.java
@@ -1,17 +1,106 @@
 package com.madmax.view;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class AuthFrame extends JFrame {
+public class AuthFrame extends JFrame implements KeyListener {
     private static AuthFrame instance;
 
+    private AuthPanel panel;
+    private JPasswordField pass;
+    private JTextField id;
+    private JButton login;
+
+
+    private AuthFrame(){
+        this.setSize(500,375);
+        this.setUndecorated(false);
+        this.setTitle("LOGIN");
+        this.setResizable(false);
+        addKeyListener(this);
+        this.panel = new AuthPanel();
+        this.panel.setLayout(new FlowLayout());
+        this.login = new JButton("Begin Hack");
+        this.id = new JTextField();
+        this.pass = new JPasswordField();
+
+        panel.add(login);
+
+        panel.add(pass);
+
+        panel.add(id);
+
+        this.add(panel);
+
+        this.setVisible(true);
+    }
+
     public static AuthFrame getInstance(){
+
         if(instance == null)
+
         {
+
             instance = new AuthFrame();
+
         }
 
 
         return instance;
+    }
+
+
+
+    public static void setInstance(AuthFrame instance) {
+        AuthFrame.instance = instance;
+    }
+
+    public AuthPanel getPanel() {
+        return panel;
+    }
+
+    public void setPanel(AuthPanel panel) {
+        this.panel = panel;
+    }
+
+    public JPasswordField getPass() {
+        return pass;
+    }
+
+    public void setPass(JPasswordField pass) {
+        this.pass = pass;
+    }
+
+    public JTextField getId() {
+        return id;
+    }
+
+    public void setId(JTextField id) {
+        this.id = id;
+    }
+
+    public JButton getLogin() {
+        return login;
+    }
+
+    public void setLogin(JButton login) {
+        this.login = login;
+    }
+
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    public void keyPressed(KeyEvent e) {
+        if ( e.getKeyCode() == KeyEvent.VK_ESCAPE ) {
+            this.dispose();
+            System.exit(0);
+        }
+    }
+
+    public void keyReleased(KeyEvent e) {
+
     }
 }
--- src/main/java/com/madmax/view/AuthPanel.java
+++ src/main/java/com/madmax/view/AuthPanel.java
@@ -3,18 +3,5 @@ package com.madmax.view;
 import javax.swing.*;
 
 public class AuthPanel extends JPanel {
-    private static AuthPanel instance;
-
-    public static AuthPanel getInstance(){
-        if(instance == null)
-        {
-            instance = new AuthPanel();
-        }
-
-
-        return instance;
-
-    }
-
 
 }
