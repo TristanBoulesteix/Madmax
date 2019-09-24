90ef69c57d550909a092e7852dd1b6fd246de748
CharlotteM <33549005+DrakLulu@users.noreply.github.com>
2019-09-24 15:17:47 +0200

    update main

--- src/main/java/com/madmax/Main.java
+++ src/main/java/com/madmax/Main.java
@@ -3,8 +3,6 @@ import com.madmax.model.CAD;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world !");
-        CAD cad = new CAD();
-        cad.connect();
+       
     }
 }
