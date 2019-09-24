2686c257e97c4d315eeaf875ca827df4171ed345
VincentOnfray <vincent.onfray@viacesi.fr>
2019-09-24 15:14:45 +0200

    Generateur de clées opérationel

--- src/main/java/com/madmax/Main.java
+++ src/main/java/com/madmax/Main.java
@@ -1,8 +1,15 @@
 package com.madmax;
 
+import com.madmax.controller.Wkf_decrypt;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.out.println("Hello world !");
+
+        while (true) {
+            System.out.println(Wkf_decrypt.getInstance().generateKeyFromLastAttempt());
+
+        }
         
     }
 }
