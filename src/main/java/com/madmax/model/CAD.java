701573c98f9dd14615eab364a6dd82ab328f878b
CharlotteM <33549005+DrakLulu@users.noreply.github.com>
2019-09-24 14:42:34 +0200

    CAD done

--- src/main/java/com/madmax/model/CAD.java
+++ src/main/java/com/madmax/model/CAD.java
@@ -1,29 +1,27 @@
 package com.madmax.model;
+//import org.apache.spark.sql.Dataset;
 import java.sql.*;
+import java.util.ArrayList;
 
 public class CAD {
 
-        public void connect() {
-            try{
-
-                Connection con=DriverManager.getConnection(
-                        "jdbc:mysql://localhost/madmax?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC","root","");
-
-                Statement stmt=con.createStatement();
-                ResultSet rs=stmt.executeQuery("select * from utilisateur");
-                while(rs.next())
-                    System.out.println(rs.getInt(1)+"  "+rs.getString(2));
-                con.close();
-            }catch(Exception e){ System.out.println("ERROR"+ e);}
+    public String getRows(String rSQL, String ResultSetName) {
+         String data = null;
+        try {
+            Connection con = DriverManager.getConnection(
+                    "jdbc:mysql://localhost/madmax?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC", "root", "");
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery(rSQL);
+            rs.next();
+            data = rs.getString(1);
+            con.close();
+        } catch (Exception e) {
+            System.out.println("ERROR" + e);
         }
-
-/* http://localhost/phpmyadmin/db_structure.php?server=1&db=madmax
-    public DataSet getRows(String rSQL, String ResultSetName){
-
-        return Dataset data;
+        return data ;
     }
 
     public void actionRows(String rqSQL){
 
-    } */
+    }
 }
--- src/main/java/com/madmax/model/MapP.java
+++ src/main/java/com/madmax/model/MapP.java
@@ -1,9 +1,15 @@
 package com.madmax.model;
 
+import java.sql.ResultSet;
+
 public class MapP {
 
-    public String selectIDbyLoginPassword(String login, String Password){
+    public String selectIDbyLoginPassword(String login, String password){
 
-        return "true/fasle";
+        CAD cadP = new CAD();
+        String data = cadP.getRows("SELECT ID FROM utilisateur WHERE Login =\""+login+"\" AND Password =\""+password+"\";", "user");
+
+        System.out.println(data);
+        return data;
     }
 }
