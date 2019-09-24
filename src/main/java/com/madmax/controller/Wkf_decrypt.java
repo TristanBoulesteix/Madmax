f8639fbbfdab4c2357dff5fb2fe3c253e6d85794
TristanBoulesteix <tristan.boulesteix@gmail.com>
2019-09-24 10:05:23 +0200

    Create Wkf_decrypt.java

---
+++ src/main/java/com/madmax/controller/Wkf_decrypt.java
@@ -0,0 +1,22 @@
+package com.madmax.controller;
+
+
+import com.madmax.model.Decrypt;
+import com.madmax.model.Files;
+
+public class Wkf_decrypt {
+    private final static Wkf_decrypt INSTANCE = new Wkf_decrypt();
+
+    public static Wkf_decrypt getInstance() {
+        return INSTANCE;
+    }
+
+    private Wkf_decrypt() {
+    }
+
+    public boolean pcs_decrypter(String source_path, String destination_path) {
+        String data = Decrypt.getInstance().decrypt(Files.getInstance().getData(source_path), "key");
+        Files.getInstance().setData(destination_path, data);
+        return true;
+    }
+}
