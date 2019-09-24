5028925b699e9f229cd5e53718e60438d58def90
CharlotteM <33549005+DrakLulu@users.noreply.github.com>
2019-09-24 10:33:12 +0200

    script sql

--- planif/script.sql
+++ planif/script.sql
@@ -2,22 +2,20 @@
 #        Script MySQL.
 #------------------------------------------------------------
 
+CREATE DATABASE madmax;
 
 #------------------------------------------------------------
 # Table: Utilistateur
 #------------------------------------------------------------
-
+SELECT madmax,
 CREATE TABLE Utilistateur(
         ID       Int  Auto_increment  NOT NULL ,
         Login    Varchar (50) NOT NULL ,
         Password Varchar (5) NOT NULL
 )ENGINE=InnoDB;
-
-
 #------------------------------------------------------------
 # Table: Dictionnaire
 #------------------------------------------------------------
-
 CREATE TABLE Dictionnaire(
         ID  Int  Auto_increment  NOT NULL ,
         Mot Varchar (50) NOT NULL
