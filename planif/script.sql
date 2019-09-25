d70ac6b0b1b94c5255c122660e7cf532b57b8cf3
FOURNEL Maxime <33550897+WustFournel@users.noreply.github.com>
2019-09-25 09:18:48 +0200

    Update script.sql

--- planif/script.sql
+++ planif/script.sql
@@ -2,26 +2,44 @@
 #        Script MySQL.
 #------------------------------------------------------------
 
-CREATE DATABASE madmax;
+#------------------------------------------------------------
+# Database: madmax
+#------------------------------------------------------------
+
+DROP DATABASE IF EXISTS madmax;
+CREATE DATABASE IF NOT EXISTS madmax CHARACTER SET UTF8 collate utf8_bin;
 
 #------------------------------------------------------------
 # Table: Utilistateur
 #------------------------------------------------------------
-SELECT madmax,
-CREATE TABLE Utilisateur(
-        ID       Int  PRIMARY KEY  NOT NULL AUTO_INCREMENT,
-        Login    Varchar (50) NOT NULL ,
-        Password Varchar (5) NOT NULL
-)ENGINE=InnoDB;
+
+CREATE TABLE IF NOT EXISTS madmax.Utilisateur
+(
+    ID       Int  PRIMARY KEY  NOT NULL AUTO_INCREMENT,
+    Login    Varchar (50) NOT NULL ,
+    Password Varchar (5) NOT NULL
+)
+ENGINE=InnoDB;
+
 #------------------------------------------------------------
 # Table: Dictionnaire
 #------------------------------------------------------------
-CREATE TABLE Dictionnaire(
-        ID  Int  PRIMARY KEY  NOT NULL AUTO_INCREMENT,
-        Mot Varchar (50) NOT NULL
-)ENGINE=InnoDB;
 
-INSERT INTO Utilistateur(Login, Password) VALUES ("root", "Hello"),("Admin", "Admin");
+CREATE TABLE IF NOT EXISTS madmax.Dictionnaire
+(
+    ID  Int  PRIMARY KEY  NOT NULL AUTO_INCREMENT,
+    Mot Varchar (50) NOT NULL
+)
+ENGINE=InnoDB;
 
-INSERT INTO Dictionnaire(Mot) VALUES ("trucc"),("Hello"),("password"),("patate");
+INSERT INTO madmax.Utilisateur(Login, Password) 
+	VALUES 
+	("root", "Hello"),
+	("Admin", "Admin");
 
+INSERT INTO madmax.Dictionnaire(Mot) 
+	VALUES 
+	("trucc"),
+	("Hello"),
+	("password"),
+	("patate");
