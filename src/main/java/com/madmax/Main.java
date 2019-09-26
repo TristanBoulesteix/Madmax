16b4b59e28b981b062969d6efd94d046d87a8591
TristanBoulesteix <tristan.boulesteix@gmail.com>
2019-09-26 11:41:34 +0200

    Update Main.java

--- src/main/java/com/madmax/Main.java
+++ src/main/java/com/madmax/Main.java
@@ -6,13 +6,15 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        try {
-            UIManager.setLookAndFeel(
-                    UIManager.getSystemLookAndFeelClassName());
-        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException e) {
-            e.printStackTrace();
-        }
+        SwingUtilities.invokeLater(() -> {
+            try {
+                UIManager.setLookAndFeel(
+                        UIManager.getSystemLookAndFeelClassName());
+            } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException e) {
+                e.printStackTrace();
+            }
 
-        Wkf_cpte.getInstance().showLoginFrame(false);
+            Wkf_cpte.getInstance().showLoginFrame(false);
+        });
     }
 }
