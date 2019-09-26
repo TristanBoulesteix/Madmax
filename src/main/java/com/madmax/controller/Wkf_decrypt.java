8a2a9d3499aa1058d89e456bd1a7f972076f7983
VincentOnfray <vincent.onfray@viacesi.fr>
2019-09-26 11:58:42 +0200

    Amelioration des sorties
    et reduction du nombre de requetes SQL

--- src/main/java/com/madmax/controller/Wkf_decrypt.java
+++ src/main/java/com/madmax/controller/Wkf_decrypt.java
@@ -6,6 +6,7 @@ import com.madmax.model.Files;
 import com.madmax.model.MapDic;
 
 public class Wkf_decrypt {
+    int nbTested;
     private String lastTestKey;
     private static final String firstKey = "aaaaaaaa"; //The first key to be tested and incremented
     private static final String hint = "awqp"; //4 fixed letters that were given to us
@@ -16,6 +17,7 @@ public class Wkf_decrypt {
     }
 
     private Wkf_decrypt() {
+        this.nbTested = 0;
         this.lastTestKey = firstKey; //our first key tested, then we'll increment it to go forward                           /!\12 chars/!\
     }
 
@@ -28,7 +30,7 @@ public class Wkf_decrypt {
         return true;
     } //Main method, looks for a suitable key to decipher the message
 
-    public void testKeyOnFile(String pathIn, String pathOut, String key) {
+    private void testKeyOnFile(String pathIn, String pathOut, String key) {
 
         String data = Files.getInstance().getData(pathIn);
         String out = Decrypt.getInstance().decrypt(data, key);
@@ -39,13 +41,13 @@ public class Wkf_decrypt {
         } else {
 
         }
-        out = "\n" + key + " " + frenchNess + " : " + out;
+        out = "\n key N°"+nbTested +" "+ key  + " : "+ frenchNess + "/5 : " + out;
         System.out.println(out);
 
-
+        nbTested++;
     } //tests a key and saves it if it looks like it works
 
-    public String generateKeyFromLastAttempt() {
+    private String generateKeyFromLastAttempt() {
         int length = lastTestKey.length();
         incrementChar(length - 1);
         return hint + lastTestKey;
@@ -83,7 +85,7 @@ public class Wkf_decrypt {
         }
     } // increments individual chars in the key as needed (if a char is "z", the one before it goes up one char)
 
-    public String[] getFirstWords(String text) { //Returns the 5 first words of a string in an array, after removing Uppercase chars and punctuation
+    private String[] getFirstWords(String text) { //Returns the 5 first words of a string in an array, after removing Uppercase chars and punctuation
 
         String[] words = text.split(" ", 6);
 
@@ -96,12 +98,11 @@ public class Wkf_decrypt {
     }
 
 
-    public int rateMyFrench(String[] words) {
+    private int rateMyFrench(String[] words) {
         int length = words.length - 1;
         int correct = 0;
         int wrong = 0;
         for (int i = 0; i < length; i++) {
-            System.out.println(words[i]);
             if (isWordFrench(words[i])) {
                 correct++;
             } else {
@@ -112,12 +113,20 @@ public class Wkf_decrypt {
     } //Counts the number of french words in an array
 
     private boolean isWordFrench(String word) { //Is a word french? this method returns the answer according to our "really reliable" dictionary
-        String result = MapDic.getInstance().selectWord(word);
-        if (result == null) {
-            return false;
-        } else {
-            return true;
+        if (isAlpha(word)) {
+            String result = MapDic.getInstance().selectWord(word);
+            if ( result == null ) {
+
+            } else {
+                return true;
+            }
+
         }
+        return false;
+    }
+
+    private boolean isAlpha(String name) {
+        return name.matches("[a-zA-Z]+");
     }
 
 }
